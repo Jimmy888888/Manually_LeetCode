89284ad935a0d8f4a5c2ae510c98a33040c35e00
Kang Jen-Chun <65431800+Jimmy888888@users.noreply.github.com>
2025-07-10 17:00:41 +0800

    Update and rename solution.cpp to solution.h

---
+++ unit_test_hello_world/cpp/solution.h
@@ -0,0 +1,26 @@
+struct TreeNode{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(): val(0), left(nullptr), right(nullptr){}                                      // Constructor
+  TreeNode(int val): val(val), left(nullptr), right(nullptr){}                             // Constructor overloading
+  TreeNode(int val, TreeNode *left, TreeNode *right): val(val), left(left), right(right){} // Constructor overloading
+};
+
+class Solution{
+  public:
+    bool isSymmetric(TreeNode *root){
+      if(!root){return true;}
+
+      return isMirror(root->left, root->right);
+    }
+
+    bool isMirror(TreeNode *t1, TreeNode *t2){
+      if(!t1 && !t2){return true;}
+      if(!t1 || !t2){return false;}
+
+      return (t1->val == t2->val) &&
+        isMirror(t1->left, t2->right) &&
+        isMirror(t1->right, t2->left);
+    }
+};
