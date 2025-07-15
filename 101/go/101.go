02a39ac087bec68966e706e204c94d370f4acfa0
Kang Jen-Chun <65431800+Jimmy888888@users.noreply.github.com>
2025-07-15 22:05:46 +0800

    go 101.go

--- 101/go/101.go
+++ 101/go/101.go
@@ -1,1 +1,26 @@
 
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+    if root == nil {
+        return true
+    }
+
+    return isMirror(root.Left, root.Right)
+}
+
+func isMirror(t1 *TreeNode, t2 *TreeNode) bool {
+    if t1 == nil && t2 == nil {
+        return true
+    }
+
+    if t1 == nil || t2 == nil {
+        return false
+    }
+
+    return (t1.Val == t2.Val) && isMirror(t1.Left, t2.Right) && isMirror(t1.Right, t2.Left)
+}
