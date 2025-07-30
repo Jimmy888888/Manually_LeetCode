ebca7dd4f8096763c7ed46dfcc6eb6b101ff9023
Kang Jen-Chun <65431800+Jimmy888888@users.noreply.github.com>
2025-07-30 23:02:36 +0800

    LeetCode 55, thought of solving and finish 55.go

--- 55/README.md
+++ 55/README.md
@@ -1,3 +1,42 @@
-You are given an integer array `nums`. You are initially positioned at the array's **first index**, and each element in the array represents your maximum jump length at that position.
+greedy : in every step, choose the biggest number
 
-Return `true` if you can reach the last index, or `false` otherwise.
+success condition : the position reach the last index
+
+## values :
+
+- step : the step of the current position
+
+- newStep : the step of the next position
+
+- position : the position of the current position
+
+
+## how to find the next position
+
+#### way 1 : simlply find the biggest number in the range of step
+
+```GO
+for j := i+1; j <= i + step; j++ {
+    if newStep < nums[j] {
+        newStep = nums[j]
+        position = j
+    }
+}
+```
+
+result : fail, because the partly biggest number may not be the longest step
+
+#### way 2 : include the distance, using the (i + step) as the stander
+
+```GO
+for j := i+1; j <= i + step; j++ {
+    if j + nums[j] >= i + step {
+        newStep = nums[j]
+        position = j
+        break
+    }
+}
+```
+result : success
+
+
--- 55/go/55.go
+++ 55/go/55.go
@@ -1,14 +1,46 @@
 package jumpgame
 
+import "fmt"
+
 func canJump(nums []int) bool {
-	reachable := 0
-	for i, n := range nums {
-		if i > reachable {
-			return false
-		}
-		if i+n > reachable {
-			reachable = i + n
-		}
-	}
-	return true
+
+    i := 0
+    step := nums[0]
+    newStep := step
+    position := 0
+    for i < len(nums){
+        // check success
+        if i + step >= len(nums) - 1 {
+            return true
+        }else if i + step <= i {
+            return false
+        }
+        
+        // find the next position
+        newStep = 0
+        position = i+1
+        for j := i+1; j <= i + step; j++ {
+            if j + nums[j] >= i + step {
+                newStep = nums[j]
+                position = j
+                break
+                // fmt.Print(position)
+                // fmt.Print(newStep)
+            }
+        }
+
+        step = newStep
+
+        i = position
+        // fmt.Print(i)
+        // fmt.Print(step)
+    }
+
+    return true
 }
+
+// greedy : in every step, choose the biggest number
+// success condition : the position reach the last index
+// values :
+// // step : 
+// // position : 
