249e23ed0706a326417d663e611ad6703f1c6ca2
Kang Jen-Chun <65431800+Jimmy888888@users.noreply.github.com>
2025-07-31 23:24:14 +0800

    finish 55.py

--- 55/py/55.py
+++ 55/py/55.py
@@ -2,10 +2,28 @@ from typing import List
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        reachable = 0
-        for i, n in enumerate(nums):
-            if i > reachable:
+        step = nums[0]
+        newStep = 0
+        position = 0
+        i = 0
+        while i < len(nums) :
+            # check success
+            if i + step >= len(nums) - 1 :
+                return True
+            elif i + step <= i :
                 return False
-            if i + n > reachable:
-                reachable = i + n
+            
+            newStep = 0
+            position = i + 1
+            for j in range(i+1, i+step+1) :
+                if j + nums[j] >= i + step:
+                    newStep = nums[j]
+                    position = j
+                    break
+            
+
+            i = position
+            step = newStep
+
         return True
+            
