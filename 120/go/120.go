f2cbee533c3b98ed8704761149f61b10d41619cb
Kang Jen-Chun <65431800+Jimmy888888@users.noreply.github.com>
2025-08-12 21:20:48 +0800

    120 go solution

---
+++ 120/go/120.go
@@ -0,0 +1,15 @@
+func minimumTotal(triangle [][]int) int {
+    for i:=len(triangle) - 2; i>=0; i-- {
+        for j:=0; j<=i; j++ {
+            triangle[i][j] = triangle[i][j] + Min(triangle[i+1][j], triangle[i+1][j+1])
+        }
+    }
+    return triangle[0][0]
+}
+
+func Min(a int, b int) int {
+    if a > b {
+        return b
+    }
+    return a
+}
